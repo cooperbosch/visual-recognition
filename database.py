a0bfe530c326878b938f40aedfe27d4f7e0c448c
doubleiis02 <50722281+doubleiis02@users.noreply.github.com>
2019-07-18 16:26:04 -0400

    Update database.py

---
+++ database.py
@@ -0,0 +1,30 @@
+
+    
+
+class Profile:
+    def __init__(self, name, arr):
+        self.name = name
+        self.mean_descriptor = arr
+        self.num_of_samples = 1
+        
+        """
+        name = string
+        arr = numpy array (128 vector) corresponding to picture of face
+        
+        """
+        
+    def __call__(self, newarr):
+        total = self.mean_descriptor * self.num_of_samples + newarr
+        self.num_of_samples += 1
+        new_mean_descriptor = total / self.num_of_samples
+        return new_mean_descriptor
+    
+        """
+        newarr = 128 vector numpy array of new picture of person
+        
+        returns:
+            new_mean_descriptor = numpy array
+        
+        """
+        
+database = {}
