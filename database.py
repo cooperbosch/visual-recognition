489f6688ba459ccb47cf97bb8a09a153e9ab54e1
doubleiis02 <50722281+doubleiis02@users.noreply.github.com>
2019-07-19 10:15:22 -0400

    axis=0

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import numpy as np
 class Profile:
     def __init__(self, name, arr):
         self.name = name
-        self.mean_descriptor = np.mean(arr)
+        self.mean_descriptor = np.mean(arr, axis=0)
         self.array = arr
         
         """
@@ -15,7 +15,7 @@ class Profile:
         
     def __call__(self, newarr):
         
-        self.array = np.vstack(self.array, newarr)
+        self.array = np.vstack(self.array, newarr, axis=0)
         self.mean_descriptor = np.mean(self.array)
     
         """
