26fd2e2c852ac97b188b16c853a9bf9e45e3fedc
Cooper Bosch <coopercb333@gmail.com>
2019-07-19 09:40:03 -0400

    added descriptor function

--- TakePicture.py
+++ TakePicture.py
@@ -6,16 +6,21 @@ from dlib_models import download_model, download_predictor, load_dlib_models
 download_model()
 download_predictor()
 from dlib_models import models
+face_detect = models["face detect"]
+face_rec_model = models["face rec"]
+shape_predictor = models["shape predict"]
 
 def take_pic():
     pic = take_picture()
+
+    return(pic)
+
+def get_descriptor(pic):
     load_dlib_models()
-    face_detect = models["face detect"]
-    face_rec_model = models["face rec"]
-    shape_predictor = models["shape predict"]
+    detections = list(face_detect(pic))
     shape = shape_predictor(pic, detections[0])
     descriptor = np.array(face_rec_model.compute_face_descriptor(pic, shape))
-    return (pic, face_detect, face_rec_model, shape_predictor, descriptor)
+    return (descriptor)
 
 
 def find_rectangles(pic):
@@ -28,3 +33,14 @@ def find_rectangles(pic):
         r.append(detections[i].right() - detections[i].left())
         r.append(detections[i].bottom() - detections[i].top())
         rectangles.append(r)
+
+    # to plot rectangles use the following code:
+    '''
+    for r in rectangles:
+        rect = Rectangle((r[0], r[1]), r[2], r[3], linewidth=1, edgecolor='r', facecolor='none')
+
+        ax.add_patch(rect)
+    '''
+
+    return rectangles
+
